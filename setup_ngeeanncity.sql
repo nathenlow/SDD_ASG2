7c8aed7e9e49d144ada060fafad642ffa41d02af
nathenlow <nathen.low@gmail.com>
2022-12-09 16:07:43 +0800

    setup databse sql

---
+++ setup_ngeeanncity.sql
@@ -0,0 +1,53 @@
+﻿CREATE DATABASE IF NOT EXISTS ngeeanncity;
+USE ngeeanncity;
+
+/*--------------------------------------
+              CREATE TABLE
+--------------------------------------*/
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  userid int(11) NOT NULL AUTO_INCREMENT,
+  email varchar(320) NOT NULL,
+  password varchar(128) NOT NULL,
+  username varchar(50) NOT NULL,
+  savedgamedata mediumtext NOT NULL DEFAULT '{}' COMMENT 'JSON array',
+  PRIMARY KEY (userid),
+  UNIQUE(email),
+  UNIQUE(username)
+)
+ENGINE = INNODB;
+
+
+DROP TABLE IF EXISTS highscores;
+CREATE OR REPLACE TABLE highscores (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  score int(11) NOT NULL,
+  userid int(11) NOT NULL,
+  timestamp datetime NOT NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  CONSTRAINT FOREIGN KEY (userid) REFERENCES user(userid)
+)
+ENGINE = INNODB;
+
+
+
+
+/*--------------------------------------
+                INSERT
+--------------------------------------*/
+
+-- Dumping data for table user
+INSERT INTO user (email, password, username) VALUES
+('n@l.com', 'Qwer1234', 'nathen'),
+('j@w.com', 'Qwer1234', 'joseph'),
+('s@j.com', 'Qwer1234', 'daniel'),
+('y@c.com', 'Qwer1234', 'yuchen'),
+('z@y.com', 'Qwer1234', 'zhiyan');
+
+-- Dumping data for highscore
+INSERT INTO highscores (score, userid) VALUES
+(15, 1),
+(13, 2),
+(2, 3),
+(7, 4),
+(11, 5);
